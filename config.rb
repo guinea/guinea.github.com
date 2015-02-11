e2d912a07b56089fe62eb4e29aa5299118569bad
Duncan Steele <steeleduncan@hotmail.com>
2015-02-11 18:42:39 +0000

    attempting to convert to scss

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+css_dir = "stylesheets"
+sass_dir = "_scss"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
